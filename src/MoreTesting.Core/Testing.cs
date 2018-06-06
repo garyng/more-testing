1ab456c4a49c25e12559d0eeb375d467c5854bff
GaryNg <garyng.zhongbo@gmail.com>
2018-06-06 23:36:31 +0800

    code: add example solution

---
+++ src/MoreTesting.Core/Testing.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace MoreTesting.Core
+{
+	public class Testing
+	{
+		public string Title => "More Testing";
+		
+		[JsonIgnore]
+		public string Json => JsonConvert.SerializeObject(this, Formatting.Indented);
+
+
+	}
+}
