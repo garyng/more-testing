2c91ffc5c84008c5ec1c00172bf5bbcb8b3d5204
GaryNg <garyng.zhongbo@gmail.com>
2018-06-09 22:14:23 +0800

    code: add tests

---
+++ tests/MoreTesting.Tests/MoreTestingTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace MoreTesting.Tests
+{
+	[TestFixture]
+	public class MoreTestingTests
+	{
+		[Test]
+		public void Should_Pass_This()
+		{
+			// Arrange
+			
+
+			// Act
+
+
+			// Assert
+			Assert.Pass();
+		}
+
+		[Test]
+		[Category("IntegrationTests")]
+		public void Should_BeSkipped()
+		{
+			// Arrange
+			
+
+			// Act
+
+
+			// Assert
+			Assert.Pass();
+		}
+	}
+}
---
+++ tests/MoreTesting.Wpf.Tests/WpfTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace MoreTesting.Wpf.Tests
+{
+	[TestFixture]
+	public class WpfTests
+	{
+		[Test]
+		public void Should_Pass_This()
+		{
+			// Arrange
+
+
+			// Act
+
+
+			// Assert
+			Assert.Pass();
+		}
+
+		[Test]
+		public void Should_RandomlyFail()
+		{
+			// Arrange
+
+
+			// Act
+
+
+			// Assert
+			if (new Random().Next(100) > 50)
+			{
+				Assert.Pass();
+			}
+			else
+			{
+				Assert.Fail();
+			}
+		}
+
+		[Test]
+		[Category("IntegrationTests")]
+		public void Should_BeSkipped()
+		{
+			// Arrange
+
+
+			// Act
+
+
+			// Assert
+			Assert.Pass();
+		}
+	}
+
+}
