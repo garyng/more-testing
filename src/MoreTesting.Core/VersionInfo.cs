087a2b352c34dac4f9104fdd34c5823fa0d41bab
GaryNg <garyng.zhongbo@gmail.com>
2018-06-09 19:24:58 +0800

    code: update property

--- src/MoreTesting.Core/VersionInfo.cs
+++ src/MoreTesting.Core/VersionInfo.cs
@@ -12,10 +12,9 @@ namespace MoreTesting.Core
 	public class VersionInfo
 	{
 		public string Title => "New fancy features!";
-
 		public string AssemblyVersion => Assembly.GetExecutingAssembly().GetName().Version.ToString();
-		public string FileVersion = FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).FileVersion;
-		public string ProductVersion = FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).ProductVersion;
+		public string FileVersion => FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).FileVersion;
+		public string ProductVersion => FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).ProductVersion;
 
 		[JsonIgnore]
 		public string Json => JsonConvert.SerializeObject(this, Formatting.Indented);
